601433d237f63bb0c2e21494f0a6af3ba9ea0267
Rana Bakri <rana.bakri@rawdalsaleheen.edu.kw>
2022-10-16 16:47:48 +0300

    rana task 4

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def get_home(request):
+
+    return HTTPResponse("<h>Welcome to our site</h>")
+
+
+def get_product(request, product_id):
+    products = Product.objects.get(id=product_id)
+    return HTTPResponse(
+        f"""
+    <p>{product.id}</p>
+    <h1>{product.name}</h1>
+    <p>{product.price}</p>
+    """
+    )
--- productshop/urls.py
+++ productshop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import get_home, get_product
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", get_home, name="home"),
+    path("product/<int:product_id>", get_product, name="product_detail"),
 ]
